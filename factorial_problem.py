49709dfe0d74caace83e6ec28cdf2b7426ddaf1d
Jake130 <115515270+Jake130@users.noreply.github.com>
2023-07-31 10:42:15 -0700

    Add files via upload
    This is unsolved...

---
+++ factorial_problem.py
@@ -0,0 +1,22 @@
+def factorial(number):
+    #Let's check each inductive step with a base Case
+
+
+    #Lets put our inductive step in the return statement
+    #When resolved it should return our factorial
+    return
+
+
+
+#Equals 120
+print(factorial(5))
+
+#Equals 1
+print(factorial(1))
+
+
+"""
+You can optionally create custom outputs for zero and non-negative numbers,
+under the guise of multiple base cases/conditions that are checked
+before each inductive step.
+"""
