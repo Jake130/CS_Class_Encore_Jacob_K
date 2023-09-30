a753cd80437a8564ff3e9d2df35dffaa074ffd4d
Jake130 <115515270+Jake130@users.noreply.github.com>
2023-09-30 10:46:23 -0700

    Create functions_and_methods.py

---
+++ functions_and_methods.py
@@ -0,0 +1,5 @@
+"""Setup for working with python functions and methods"""
+
+example_list = ["banana", "apple", "orange", "mango", "watermelon"]
+example_string = "This is a long sentence and it's never gonna stooooooooooooooooop!"
+#Use any integer or float as an example
