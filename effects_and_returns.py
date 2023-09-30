6c216a41ba24a42452099108246e887878f78aaf
Jake130 <115515270+Jake130@users.noreply.github.com>
2023-09-30 11:03:01 -0700

    Update effects_and_returns.py

--- effects_and_returns.py
+++ effects_and_returns.py
@@ -4,3 +4,21 @@ example_list = ["banana", "apple", "orange", "mango", "watermelon"]
 example_string = "This is a long sentence and it's never gonna stooooooooooooooooop!"
 #Use any integer or float as an example
 
+"""
+Play with using these functions and methods:
+max()
+min()
+sorted()
+split
+
+.append() #lists
+.clear()  #clear
+.index() -kill this one
+.strip()  #strings
+.upper()  #strings
+.lower()  #strings
+"""
+
+
+#By experimenting below, categorize them as producing an effect, return, or both
+print()
