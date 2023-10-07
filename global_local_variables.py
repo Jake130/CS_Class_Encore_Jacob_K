be70016adff001eb93671e4d39735d856f76fbae
Jake130 <115515270+Jake130@users.noreply.github.com>
2023-10-07 13:02:58 -0700

    Update global_local_variables.py

--- global_local_variables.py
+++ global_local_variables.py
@@ -2,17 +2,17 @@ x = 1
 y = 2
 z = 3
 
-#Uses a local variable
+
 def MultiplyTen(x:int)->int:
     x = x * 10
     return x
 
-#Uses a global varialbe
+
 def Subtract_x(result:int)->int:
     result -= x
     return result
 
-#Changes a global variable within a function
+
 def Add5_to_y()->int:
     global y
     y = 5 + y
